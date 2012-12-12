f0612470813ad707d001e501db8468aec40412e7
Jacob Persson <jacob.persson@gmail.com>
2012-12-12 21:15:23 +0100

    Initial commit

---
+++ Favourites/Favourites/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Favourites
+//
+//  Created by Jacob Persson on 2012-12-12.
+//  Copyright (c) 2012 Jacob Persson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Favourites/Favourites/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Favourites
+//
+//  Created by Jacob Persson on 2012-12-12.
+//  Copyright (c) 2012 Jacob Persson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
